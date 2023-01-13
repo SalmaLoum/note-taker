0f88042f6876dec384888f081945ef3ec28a9281
salmaloum <goforsalma@gmail.com>
2023-01-12 16:56:59 -0800

    updated store.js

---
+++ db/store.js
@@ -0,0 +1,52 @@
+const util = require('util')
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid')
+const { notStrictEqual } = require('assert')
+
+const readFromFile = util.promisify(fs.readFile)
+const writeFromFile = util.promisify(fs.writeFile)
+
+//Reading file
+class Store {
+  readFile() {
+    return readFromFile('db/db.json', 'utf8')
+  }
+  //writing file to data
+  writeFile(data) {
+    return writeFromFile('db/db.json', JSON.stringify(data))
+  }
+  //get notes will use the info from readFile and turn it into an array
+  getNotes() {
+    return this.readFile().then((response) => {
+      var parseNotes = []
+      // if we're able to concat these two files the response once parse becomes an array.
+      if ([].concat(JSON.parse(response))) {
+        parseNotes = [].concat(JSON.parse(response))
+      } else {
+        parseNotes = []
+      }
+      return parseNotes
+    })
+  }
+
+  //
+  addNotes(data) {
+    const newNote = {
+      title: data.title,
+      text: data.text,
+      id: uuidv4(),
+    }
+    //adding the data to the json file
+    //pushing an item into my array
+    return this.getNotes()
+    //adding the notes
+      .then((response) => [...response, newNote])
+      .then((updatedNotes) => {
+        this.writeFile(updatedNotes)
+      })
+      .then(() => newNote)
+    
+   
+  }
+}
+module.exports = new Store()
